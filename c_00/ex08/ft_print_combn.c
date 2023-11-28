b2d33ce7614fd9852fb96d271805dd59c331ca3f
xmyhoang <62980803+xmyhoang@users.noreply.github.com>
2023-11-28 02:03:33 +0100

    ex08

--- c_00/ex08/ft_print_combn.c
+++ c_00/ex08/ft_print_combn.c
@@ -1,31 +1,53 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void	increment_count(int	nb_array[], int	index)
+void	output_combn(int arr[], int n)
 {
-	int	i;
-	for (i = '0'; i <= '9'; i++)
+	// Iterate through the array and print
+	for (int i = 0; i < n; i++)
 	{
-		nb_array[index] = i;
-		write(1, &nb_array[index-1], 1);
-		write(1, &nb_array[index], 1);
-		write(1, ", ", 1);
+		char	digit;
+
+		digit = arr[i] + '0';
+		write(1, &digit, 1);
+	}
+
+	// If not the end, print commas
+	if (arr[0] != 10 - n)
+	{
+		write(1, ", ", 2);
+	}
+}
+
+void	generate_combn(int arr[], int index, int n)
+{
+	// If current index is equal to the combo size, output
+	if (index == n)
+	{
+		output_combn(arr, n);
+	}
+	int	start;
+
+	// Determine the starting point. Continue from the value after previous element in the array
+	start = (index == 0) ? 0 : arr[index - 1] + 1;
+
+	for (int i = start; i <= 9; i++)
+	{
+		// Set value for current index
+		arr[index] = i;
+		// Recursive call with next index
+		generate_combn(arr, index + 1, n);
 	}
 }
 
 void	ft_print_combn(int n)
 {
-	int len;
-	int	nb_array[n];
-
-	for (len = n; len >= 0; len--)
-	{
-		increment_count(nb_array, len);
-	}
+	int	arr[n];
+	generate_combn(arr, 0, n);
 }
 
 int	main(void)
 {
-	ft_print_combn(2);
+	ft_print_combn(5);
 	return (0);
 }
